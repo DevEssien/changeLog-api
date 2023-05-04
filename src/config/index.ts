e3dbb4543db918a4abb3015ef6dbedefc0182c81
Essien Emmanuel <essienemma300@gmail.com>
2023-05-04 11:34:57 -0700

    configured the environment var

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,3 +12,14 @@ if (stage === 'production') {
 } else {
     envConfig = require('./local');
 }
+
+export default merge({
+    stage,
+    env: process.env.NODE_ENV,
+    port: 3001,
+    secrets: {
+        jwt: process.env.JWT_SECRET,
+        dbUrl: process.env.DATABASE_URL
+    }
+    
+}, envConfig)
